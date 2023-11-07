cb7eb4866eea14d739547369b1cb27d08342894b
ERO <eromail@gmail.com>
2023-11-07 00:26:02 -0300

    +FIX TODOs Unity-DelegatesExercise 1 (Timer)

--- Assets/Scripts/Exercise1-Delegates/Timer/TimerView.cs
+++ Assets/Scripts/Exercise1-Delegates/Timer/TimerView.cs
@@ -4,17 +4,31 @@ using Exercise1_Delegates.Timer;
 using TMPro;
 using UnityEngine;
 
-//TODO: Complete this class to solve all TODOs and add some nice visual behaviour to the timer.
+//TODO: (DONE)Complete this class to solve all TODOs and add some nice visual behaviour to the timer.
 public class TimerView : MonoBehaviour
 {
     [SerializeField] private TMP_Text timerNumber;
 
     [SerializeField] private SecondsCounter secondsCounter;
-    
-    //TODO: Subscribe this method to the event found in the SecondsCounter class.
-    //TODO: This method should only be subscribed to the event while the component is enabled! 
-    private void HandleSecondPassed(int currentSeconds)
+
+	//TODO: (DONE)Subscribe this method to the event found in the SecondsCounter class.
+	//TODO: (DONE)This method should only be subscribed to the event while the component is enabled! 
+
+	private void OnEnable()
+	{
+		Debug.Log($"{name} enabled!");
+		secondsCounter.onSecondPassed += HandleSecondPassed;
+	}
+
+	private void OnDisable()
+	{
+		Debug.Log($"{name} disabled!");
+		secondsCounter.onSecondPassed -= HandleSecondPassed;
+	}
+
+	private void HandleSecondPassed(int currentSeconds)
     {
-        //TODO: Update the timerNumber's text to reflect the current seconds.
-    }
+		//TODO: (DONE)Update the timerNumber's text to reflect the current seconds.
+		timerNumber.SetText(currentSeconds.ToString());
+	}
 }
